00a3b4a393f94775fa728533117b512e650a9b6a
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-05-07 10:32:02 +0700

    update

--- no1.js
+++ no1.js
@@ -1,10 +1,25 @@
 function palindrom(str) {
-  const newString = str.split("").reverse().join("")
-  console.log(newString)
-  if (str === newString) {
-    console.log(`${str} adalah kata palindrom`)
-  } else {
-    console.log(`${str} bukan kata palindrom`)
-  }
+    const newString = str.split("").reverse().join("")
+    //console.log(newString)
+    if (str === newString) {
+        console.log(`${str} adalah kata palindrom`)
+    } else {
+        console.log(`${str} bukan kata palindrom`)
+    }
 }
 palindrom("malam")
+
+function cekKata(str) {
+    let newStr = ""
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr = newStr + str.charAt(i)
+    }
+    //console.log(newStr)
+    if (str === newStr) {
+        console.log("palindrom")
+    } else {
+        console.log("bukan palindrom")
+    }
+}
+
+cekKata("malam")
--- no2.js
+++ no2.js
@@ -1,4 +1,14 @@
 function reverseWord(str) {
-  console.log(str.split(" ").reverse().join(" "))
+    console.log(str.split(" ").reverse().join(" "))
 }
 reverseWord("saya adalah rahadian")
+
+function balikKata(str) {
+    let result = ""
+    newStr = str.split(" ")
+    for (let i = newStr.length - 1; i >= 0; i--) {
+        result = result + newStr[i] + " "
+    }
+    console.log(result)
+}
+balikKata("saya adalah reza")
