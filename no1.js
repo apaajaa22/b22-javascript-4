cd78a5d6d0398ad5ee7e2ccd9f3a55c1fa2eb8b7
Rahadian Reza Rizaldy <59022153+apaajaa22@users.noreply.github.com>
2021-05-09 18:56:53 +0700

    update

--- no1.js
+++ no1.js
@@ -12,7 +12,7 @@ palindrom("malam")
 function cekKata(str) {
     let newStr = ""
     for (let i = str.length - 1; i >= 0; i--) {
-        newStr = newStr + str.charAt(i)
+        newStr = newStr + str[i]
     }
     //console.log(newStr)
     if (str === newStr) {
